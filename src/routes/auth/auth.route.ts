b45063833cb5ae13c98e8a4d0a3e5bb8ad84b938
JpMayor1 <royamsemaj@gmail.com>
2026-01-30 11:54:29 +0800

    feat: implemented login

--- src/routes/auth/auth.route.ts
+++ src/routes/auth/auth.route.ts
@@ -1,7 +1,8 @@
-import { logout, register } from "@/controllers/auth/auth.controller";
+import { login, logout, register } from "@/controllers/auth/auth.controller";
 import { Router } from "express";
 
 export const authRouter = Router();
 
 authRouter.post("/register", register);
+authRouter.post("/login", login);
 authRouter.post("/logout", logout);
